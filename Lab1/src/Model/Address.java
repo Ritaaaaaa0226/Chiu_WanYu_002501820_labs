7d8ea2a06860ac46e937734dfdd8d2e62eb1442a
wan yu chiu <wanyuchiu@wans-MacBook-Pro.local>
2025-09-11 17:37:56 -0400

    Upload Lab1

---
+++ Lab1/src/Model/Address.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author wanyuchiu
+ */
+public class Address {
+    String Streetname;
+    String UnitNum;
+    String City;
+    String zipCode;
+
+    public String getStreetname() {
+        return Streetname;
+    }
+
+    public void setStreetname(String Streetname) {
+        this.Streetname = Streetname;
+    }
+
+    public String getUnitNum() {
+        return UnitNum;
+    }
+
+    public void setUnitNum(String UnitNum) {
+        this.UnitNum = UnitNum;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getZipcode() {
+        return zipCode;
+    }
+
+    public void setZipcode(String Zipcode) {
+        this.zipCode = Zipcode;
+    }
+    
+    
+    
+}
